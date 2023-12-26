4788f4c87153e3c1cb8787241ff56389320079eb
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-26 11:53:38 +0100

    logo changed

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 <nav className=" border-gray-200 bg-black dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between  mx-auto p-2 ">
     <a className="flex items-center justify-center  space-x-3 rtl:space-x-reverse " href='#home' >
-      <Image src={'/logo.png'} width={130} height={10}/>
+      <Image src={'/logo2.png'} width={130} height={10}/>
     </a>
     <button type="button" onClick={openNavbar} className=" mr-2 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <svg className="  w-20 h-20 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
