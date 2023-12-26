efad6755cf1bbcad9b2c4f45ae4b945e801571a2
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-26 10:07:49 +0100

    fixed margin

--- app/components/Service/Service.jsx
+++ app/components/Service/Service.jsx
@@ -41,7 +41,7 @@ const Service = () => {
 
   return (
     <div className='service lg:h-screen mt-8 flex items-center justify-center' id='services'>
-  <div className='flex flex-col items-center justify-center w-4/5  lg:h-4/5  bg-black bg-opacity-40'>
+  <div className='flex flex-col items-center justify-center w-4/5  lg:h-4/5  bg-black m-4 bg-opacity-40'>
     <p className='text-white text-2xl lg:text-8xl font-semibold pt-2'>OUR SERVICES</p>
     <div className='flex justify-center  flex-wrap '>
        {businessInternationalizationData.map((dt,i)=> (
