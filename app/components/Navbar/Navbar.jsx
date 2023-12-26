780de120519593bf39bf2427e2971dfdd11b8717
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-12-26 14:31:49 +0100

    navbar fixed

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
     const [open,setOpen] = useState(false)
@@ -16,10 +17,8 @@ const Navbar = () => {
     <a className="flex items-center justify-center  space-x-3 rtl:space-x-reverse " href='#home' >
       <Image src={'/logo2.png'} width={130} height={10}/>
     </a>
-    <button type="button" onClick={openNavbar} className=" mr-2 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <svg className="  w-20 h-20 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
+    <button type="button" onClick={openNavbar} className=" mr-2 inline-flex items-center p-1 mt-2  justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <MdOutlineMenu size={50} />
     </button>
     <div className={` ${open ? 'block' : 'hidden'} w-full md:block md:w-auto `} >
       <ul className="font-medium text-xl flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
